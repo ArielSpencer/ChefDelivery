dc60fc639bffd232b2072a763c0f250d52aeaf9e
Ariel Spencer <arielspencer.tech@gmail.com>
2024-12-01 23:06:43 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Ariel Spencer on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
