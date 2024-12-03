c266e23cf7a212cf3c77a80290995333658d922a
Ariel Spencer <arielspencer.tech@gmail.com>
2024-12-02 22:43:37 -0300

    feat: add DataSourceMock

---
+++ ChefDelivery/Model/OrderType.swift
@@ -0,0 +1,14 @@
+//
+//  OrderType.swift
+//  ChefDelivery
+//
+//  Created by Ariel Spencer on 02/12/24.
+//
+
+import Foundation
+
+struct OrderType: Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+}
