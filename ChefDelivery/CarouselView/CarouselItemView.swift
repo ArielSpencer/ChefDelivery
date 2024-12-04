4bcad4791aba400b8c26f448ae66050d6f0ed185
Ariel Spencer <arielspencer.tech@gmail.com>
2024-12-03 23:22:57 -0300

    feat: add CarouselItemView

---
+++ ChefDelivery/CarouselView/CarouselItemView.swift
@@ -0,0 +1,28 @@
+//
+//  CarouselItemView.swift
+//  ChefDelivery
+//
+//  Created by Ariel Spencer on 03/12/24.
+//
+
+import SwiftUI
+
+struct CarouselItemView: View {
+    
+    let order: OrderType
+    
+    var body: some View {
+        Image(order.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    }
+}
+
+struct CarouselItemView_Preview: PreviewProvider {
+    static var previews: some View {
+        CarouselItemView(order: OrderType(id: 1, name: "", image: "barbecue-banner"))
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
