f0896294b9da6c912f22211a0746a02bfca03833
Ariel Spencer <arielspencer.tech@gmail.com>
2024-12-04 23:17:40 -0300

    feat: add StoreItemView

---
+++ ChefDelivery/StoresView/StoreItemView.swift
@@ -0,0 +1,33 @@
+//
+//  StoreItemView.swift
+//  ChefDelivery
+//
+//  Created by Ariel Spencer on 04/12/24.
+//
+
+import SwiftUI
+
+struct StoreItemView: View {
+    
+    let order: OrderType
+    
+    var body: some View {
+        HStack {
+            Image(order.image)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(25)
+                .frame(width: 50, height: 50)
+            
+            Text(order.name)
+                .font(.subheadline)
+        }
+    }
+}
+
+struct StoreItemView_Preview: PreviewProvider {
+    static var previews: some View {
+        StoreItemView(order: OrderType(id: 1, name: "Monstro Burger", image: "monstro-burger-logo"))
+            .previewLayout(.sizeThatFits)
+    }
+}
